3c95d10d24d7b128a42d2eec5035f2662ff26ffe
Hossam Tarek <hossamtarek250@gmail.com>
2018-08-05 19:03:06 +0200

    Add new product when clicking on save menu option

--- app/src/main/java/com/example/hossam/inventory/EditorActivity.java
+++ app/src/main/java/com/example/hossam/inventory/EditorActivity.java
@@ -1,20 +1,76 @@
 package com.example.hossam.inventory;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.hossam.inventory.data.InventoryDbHelper;
+import com.example.hossam.inventory.data.InventoryContract.ProductEntry;
 
 public class EditorActivity extends AppCompatActivity {
+    private EditText mProductNameEditText;
+    private EditText mProductPriceEditText;
+    private EditText mProductQuantityEditText;
+    private EditText mSupplierNameEditText;
+    private EditText mSupplierPhoneNumberEditText;
+
+    private InventoryDbHelper mInventoryDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        mProductNameEditText = findViewById(R.id.product_name_edit_text);
+        mProductPriceEditText = findViewById(R.id.product_price_edit_text);
+        mProductQuantityEditText = findViewById(R.id.product_quantity_text);
+        mSupplierNameEditText = findViewById(R.id.supplier_name_edit_text);
+        mSupplierPhoneNumberEditText = findViewById(R.id.supplier_phone_number_edit_text);
+
+        mInventoryDbHelper = new InventoryDbHelper(this);
+    }
+
+    private void insertProduct() {
+        String productName = mProductNameEditText.getText().toString().trim();
+        double productPrice = Double.parseDouble(mProductPriceEditText.getText().toString().trim());
+        int productQuantity = Integer.parseInt(mProductQuantityEditText.getText().toString().trim());
+        String supplierName = mSupplierNameEditText.getText().toString().trim();
+        String supplierPhoneNumber = mSupplierPhoneNumberEditText.getText().toString().trim();
+
+        ContentValues values = new ContentValues();
+        values.put(ProductEntry.COLUMN_PRODUCT_NAME, productName);
+        values.put(ProductEntry.COLUMN_PRODUCT_PRICE, productPrice);
+        values.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, productQuantity);
+        values.put(ProductEntry.COLUMN_SUPPLIER_NAME, supplierName);
+        values.put(ProductEntry.COLUMN_SUPPLIER_PHONE_NUMBER, supplierPhoneNumber);
+
+        SQLiteDatabase database = mInventoryDbHelper.getWritableDatabase();
+        long id = database.insert(ProductEntry.TABLE_NAME, null, values);
+
+        if (id != -1) {
+            Toast.makeText(this, "Error with saving product", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "New product added", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_editor, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_save) {
+            insertProduct();
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
