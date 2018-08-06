73b9cdd804f8f535e95ef179bddfbc282e98d0cc
Hossam Tarek <hossamtarek250@gmail.com>
2018-08-06 12:32:59 +0200

    Validate data before saving it

--- app/src/main/java/com/example/hossam/inventory/EditorActivity.java
+++ app/src/main/java/com/example/hossam/inventory/EditorActivity.java
@@ -1,6 +1,7 @@
 package com.example.hossam.inventory;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +20,12 @@ public class EditorActivity extends AppCompatActivity {
     private TextInputLayout mSupplierNameTextInput;
     private TextInputLayout mSupplierPhoneNumberTextInput;
 
+    private String mProductName;
+    private double mProductPrice;
+    private int mProductQuantity;
+    private String mSupplierName;
+    private String mSupplierPhoneNumber;
+
     private InventoryDbHelper mInventoryDbHelper;
 
     @Override
@@ -36,18 +43,12 @@ public class EditorActivity extends AppCompatActivity {
     }
 
     private void insertProduct() {
-        String productName = mProductNameTextInput.getEditText().getText().toString().trim();
-        double productPrice = Double.parseDouble(mProductPriceTextInput.getEditText().getText().toString().trim());
-        int productQuantity = Integer.parseInt(mProductQuantityTextInput.getEditText().getText().toString().trim());
-        String supplierName = mSupplierNameTextInput.getEditText().getText().toString().trim();
-        String supplierPhoneNumber = mSupplierPhoneNumberTextInput.getEditText().getText().toString().trim();
-
         ContentValues values = new ContentValues();
-        values.put(ProductEntry.COLUMN_PRODUCT_NAME, productName);
-        values.put(ProductEntry.COLUMN_PRODUCT_PRICE, productPrice);
-        values.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, productQuantity);
-        values.put(ProductEntry.COLUMN_SUPPLIER_NAME, supplierName);
-        values.put(ProductEntry.COLUMN_SUPPLIER_PHONE_NUMBER, supplierPhoneNumber);
+        values.put(ProductEntry.COLUMN_PRODUCT_NAME, mProductName);
+        values.put(ProductEntry.COLUMN_PRODUCT_PRICE, mProductPrice);
+        values.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, mProductQuantity);
+        values.put(ProductEntry.COLUMN_SUPPLIER_NAME, mSupplierName);
+        values.put(ProductEntry.COLUMN_SUPPLIER_PHONE_NUMBER, mSupplierPhoneNumber);
 
         SQLiteDatabase database = mInventoryDbHelper.getWritableDatabase();
         long id = database.insert(ProductEntry.TABLE_NAME, null, values);
@@ -59,6 +60,108 @@ public class EditorActivity extends AppCompatActivity {
         }
     }
 
+    private String getInput(TextInputLayout textInputLayout) {
+        return textInputLayout.getEditText().getText().toString().trim();
+    }
+
+    private boolean isInputEmpty(String input) {
+        if (input == null || input.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean validateProductName() {
+        String productNameText = getInput(mProductNameTextInput);
+
+        if (isInputEmpty(productNameText)) {
+            mProductNameTextInput.setError(getString(R.string.err_msg_product_name));
+            return false;
+        } else if (productNameText.length() > 50) {
+            mProductNameTextInput.setError(getString(R.string.err_msg_text_too_long));
+            return false;
+        }
+
+        mProductName = productNameText;
+        mProductNameTextInput.setError(null);
+        return true;
+    }
+
+    private boolean validateProductPrice() {
+        String productPriceText = getInput(mProductPriceTextInput);
+
+        if (isInputEmpty(productPriceText)) {
+            mProductPriceTextInput.setError(getString(R.string.err_msg_product_price));
+            return false;
+        }
+
+        try {
+            mProductPrice = Double.parseDouble(productPriceText);
+        } catch (NumberFormatException e) {
+            mProductPriceTextInput.setError(getString(R.string.err_msg_non_valid_number));
+            return false;
+        }
+
+        mProductPriceTextInput.setError(null);
+        return true;
+    }
+
+    private boolean validateProductQuantity() {
+        String productQuantityText = getInput(mProductQuantityTextInput);
+
+        if (isInputEmpty(productQuantityText)) {
+            mProductQuantityTextInput.setError(getString(R.string.err_msg_product_quantity));
+            return false;
+        }
+
+        try {
+            mProductQuantity = Integer.parseInt(productQuantityText);
+        } catch (NumberFormatException e) {
+            mProductQuantityTextInput.setError(getString(R.string.err_msg_non_valid_number));
+            return false;
+        }
+
+        mProductQuantityTextInput.setError(null);
+        return true;
+    }
+
+    private boolean validateSupplierName() {
+        String supplierNameText = getInput(mSupplierNameTextInput);
+
+        if (isInputEmpty(supplierNameText)) {
+            mSupplierNameTextInput.setError(getString(R.string.err_msg_supplier_name));
+            return false;
+        } else if (supplierNameText.length() > 50) {
+            mSupplierNameTextInput.setError(getString(R.string.err_msg_text_too_long));
+            return false;
+        }
+
+        mSupplierName = supplierNameText;
+        mSupplierNameTextInput.setError(null);
+        return true;
+    }
+
+    private boolean validateSupplierPhoneNumber() {
+        String supplierPhoneNumberText = getInput(mSupplierPhoneNumberTextInput);
+
+        if (isInputEmpty(supplierPhoneNumberText)) {
+            mSupplierPhoneNumberTextInput.setError(getString(R.string.err_msg_supplier_phone_number));
+            return false;
+        }
+
+        mSupplierPhoneNumber = supplierPhoneNumberText;
+        mSupplierPhoneNumberTextInput.setError(null);
+        return true;
+    }
+
+    private boolean checkInput() {
+        if (validateProductName() & validateProductPrice() & validateProductQuantity() &
+                validateSupplierName() & validateSupplierPhoneNumber()) {
+            return true;
+        }
+        return false;
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_editor, menu);
@@ -68,8 +171,11 @@ public class EditorActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.action_save) {
-            insertProduct();
-            finish();
+            if (checkInput()) {
+                insertProduct();
+                finish();
+            }
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
