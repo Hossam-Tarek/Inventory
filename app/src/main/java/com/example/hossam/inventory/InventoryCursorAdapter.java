7d9c2dbe4f8ea2e72ab7c78afa679507725f2449
Hossam Tarek <hossamtarek250@gmail.com>
2018-08-10 17:01:32 +0200

    Implement cursor adapter

---
+++ app/src/main/java/com/example/hossam/inventory/InventoryCursorAdapter.java
@@ -0,0 +1,39 @@
+package com.example.hossam.inventory;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.hossam.inventory.data.InventoryContract.ProductEntry;
+
+public class InventoryCursorAdapter extends CursorAdapter {
+
+    public InventoryCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item, viewGroup, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView NameTextView = view.findViewById(R.id.product_name_text_view);
+        TextView priceTextView = view.findViewById(R.id.price_text_view);
+        TextView quantityTextView = view.findViewById(R.id.quantity_text_view);
+
+        String name = cursor.getString(cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_NAME));
+        NameTextView.setText(name);
+
+        double price = cursor.getDouble(cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_PRICE));
+        priceTextView.setText(String.valueOf(price));
+
+        int quantity = cursor.getInt(cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_QUANTITY));
+        quantityTextView.setText(quantity);
+    }
+}
