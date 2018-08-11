79df2fe995d60025d2e67ccf9f90c983a0538ce9
Hossam Tarek <hossamtarek250@gmail.com>
2018-08-11 08:14:29 +0200

    Add delete all products option menu in catalog activity

--- app/src/main/java/com/example/hossam/inventory/CatalogActivity.java
+++ app/src/main/java/com/example/hossam/inventory/CatalogActivity.java
@@ -1,18 +1,22 @@
 package com.example.hossam.inventory;
 
+import android.app.AlertDialog;
 import android.app.LoaderManager;
 import android.content.ContentUris;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.hossam.inventory.data.InventoryContract.ProductEntry;
 
@@ -80,5 +84,57 @@ public class CatalogActivity extends AppCompatActivity
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
         mCursorAdapter.swapCursor(null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_delete_all) {
+            showDeleteConfirmationDialog();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showDeleteConfirmationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.dialog_msg_delete_all_products);
+        builder.setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                deleteAllProducts();
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (dialogInterface != null) {
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void deleteAllProducts() {
+        int rowsDeleted = getContentResolver().delete(
+                ProductEntry.CONTENT_URI,
+                null,
+                null
+        );
+
+        if (rowsDeleted != 0) {
+            Toast.makeText(this, getString(R.string.delete_all_products_successful),
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, getString(R.string.delete_all_products_failed),
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
